66936e50f8f3cbcac7638afab2cdd06e787c1a53
Julien <julien.thomas84@gmail.com>
2021-01-13 10:30:46 +0100

    init of minbase project

---
+++ src/module-info.java
@@ -0,0 +1,14 @@
+module lab3 {
+	exports iterator;
+	exports iterator.sm_join_assign_src;
+	exports tests;
+	exports btree.btree_javadoc_src;
+	exports chainexception;
+	exports catalog;
+	exports diskmgr;
+	exports index;
+	exports global;
+	exports btree;
+	exports heap;
+	exports bufmgr;
+}
